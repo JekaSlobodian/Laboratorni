311d47040c7774d696531963c73f7b9776fcda67
JekaSlobodian <127793994+JekaSlobodian@users.noreply.github.com>
2023-04-24 14:20:27 +0300

    Update Lab2part3 (1).c

--- Lab2part3 (1).c
+++ Lab2part3 (1).c
@@ -1,20 +1,21 @@
+
 #include <stdio.h>//підключення бібліотек
 #include<locale.h>
 #include<math.h>
-int t;// оголошення глобальної змінної
+float t;// оголошення глобальної змінної
 // оголошення функції
 int main() {
 int z,y; /* оголошення локальних
 змінних */
 setlocale (LC_CTYPE,"ua"); /*підключення
 підтримки кирилиці української мови*/
-printf("\n введіть перше число z="); /*
-виведення даних*/
-scanf("%d",&z);/*введення даних*/
-printf("\n введіть друге число  y=");
-scanf("%d",&y);
+ printf( "\nЗнайт значення функції t=sqrt(3*(y/(z-2*y)))-y*z "); 
+  printf("\n введіть перше число z=");
+    scanf("%d",&z);
+  printf("\n введіть перше число y=");
+    scanf("%d",&y);
 t=sqrt(3*(y/(z-2*y)))-y*z;
 printf("z\t y\t t\t \n");
-printf("%d\t%d\t%d\t \n",z,y,t);
-
+printf("%d\t%d\t%.2lf\t \n",z,y,t );
+printf(" Відповідь t=  %.2lf\t",t);
 return 0;}

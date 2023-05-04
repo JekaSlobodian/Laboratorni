0f01542d16107b413e9521e5e1e7fb0395fc3c8e
JekaSlobodian <127793994+JekaSlobodian@users.noreply.github.com>
2023-05-04 15:46:54 +0300

    Update lab3_2.c

--- lab3_2.c
+++ lab3_2.c
@@ -12,17 +12,16 @@ int main () {
 
     printf("Eвведіть кооординати точки C (x3, y3): \n ");
     scanf("%f %f", &x3, &y3);
-  distA= x1, y1,
     distB = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     distC = sqrt(pow(x3 - x1, 2) + pow (y3 - y1, 2));
-    if ( distB < distC) {
+         if (x1==x2 && x2==x3&&y1==y2&&y2==y3){
+          printf("координат однакові") ;}
+          else  if (distB == distC) {
+        printf("відстань між B i C однакова\n");}
+   else if ( distB < distC) {
         printf("точка B (%.2f, %.2f) розміщена ближче до точки A.\n" , x2 , y2);
         printf("дистанція від B до A: %.2f\n", distB);
     } 
-      if (distB == distC==distA) {
-        printf("всі точки в одному місці");
-          
-      }
     else {
         printf("Точка C (%.2f, %.2f) розміщена ближче до точки A.\n", x3,y3);
         printf("дистанція від C до A: %.2f\n", distC);
